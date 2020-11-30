e59760ab33e22e2255210705a86cbe432b4c93a3
Aaron Boodoo <aaronboodoo@live.com>
2020-11-30 12:54:58 -0400

    David Changes, home page, runner code

---
+++ src/main/java/advising/AdvisingContext.java
@@ -0,0 +1,11 @@
+package advising;
+
+/**
+ * This is  needed for the Strategy design pattern. Not Sure how to use it yet.
+ * 
+ * @author David Deonarine
+ */
+
+public class AdvisingContext {
+    
+}
