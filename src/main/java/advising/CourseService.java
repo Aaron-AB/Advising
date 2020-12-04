1c30923503795fe147435f9a9dac2fc5be1402c0
Amy Ramesar <amy.ramesar@my.uwi.edu>
2020-12-03 21:42:25 -0400

    Project Code 3/12

--- src/main/java/advising/CourseService.java
+++ src/main/java/advising/CourseService.java
@@ -13,19 +13,25 @@ public class CourseService {
     private ArrayList<Course> itCourses;
     private ArrayList<Course> csCourses;
     private ArrayList<Course> level1Courses;
+
+    /**@return Initializes and fetches all courses on instantiation */
     public CourseService() {
         this.itCourses = new ArrayList<Course>();
         this.csCourses = new ArrayList<Course>();
         this.level1Courses =new ArrayList<Course>();
         this.fetchCourses();
     }
-    
+
+    /**@return Calls the fetchDegree function with the necessary json files */
     public void fetchCourses() {
         itCourses = this.fetchDegree("itSpecialL2Courses.json", "itSpecialL2Courses");
         csCourses = this.fetchDegree("csSpecialL2Courses.json", "csSpecialL2Courses");
         level1Courses = this.fetchDegree("level1Courses.json","level1Courses");
     }
     
+    /**@param pathName the path to the json file 
+     * @param arrayName the name of the json array present in the json file
+     * @return Arraylist of courses that the json file contains */
     public ArrayList<Course> fetchDegree(String pathName, String arrayName) {
         ArrayList<Course> courses = new ArrayList<Course>();
         String contents = null;
@@ -48,19 +54,23 @@ public class CourseService {
         }
         return courses;
     }
-    
+
+    /**@return Fetches the information technology courses*/
     public ArrayList<Course> getIT() {
         return this.itCourses;
     }
     
+    /**@return Fetches the computer science courses*/
     public ArrayList<Course> getCS() {
         return this.csCourses;
     }
 
-      public ArrayList<Course> getLevel1() {
+    /**@return Fetches all level 1 courses for both Computer Science and Information Technology*/
+    public ArrayList<Course> getLevel1() {
         return this.level1Courses;
     } 
     
+    /**@return Prints the level 1 courses available*/
     public static void main(String[] args) throws IOException {
         CourseService cserv = new CourseService();
         System.out.println(cserv.getLevel1());
